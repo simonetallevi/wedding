203c93a49d12cba6f6995f549eaf638c1576f10a
Simone Tallevi-Diotallevi <simone.tallevi@pirelli.com>
2017-10-28 18:18:58 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-ncftpput -R -u $WEDDING_USERNAME -p $WEDDING_PASSWORD $WEDDING_HOST "/" "web/"
+cd web
+ncftpput -R -u $WEDDING_USERNAME -p $WEDDING_PASSWORD $WEDDING_HOST "/" "."
